dc56837128b31d75a30c25865f98089c0c86bdce
Atharva_Kasture <pkmn.atharva@gmail.com>
2024-12-10 11:53:40 -0500

    Updated rotate() method to work properly.

--- player.py
+++ player.py
@@ -20,8 +20,8 @@ class Player(CircleShape):
         white = (255,255,255)
         pygame.draw.polygon(screen, white, self.triangle(),2)
 
-    def rotate(self,dt):
-        self.rotation += PLAYER_TURN_SPEED*dt
+    def rotate(self,dt, direction):
+        self.rotation += direction*PLAYER_TURN_SPEED*dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
