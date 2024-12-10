0b257cbb66c3333e091139ad701410cca3e11a58
Atharva_Kasture <pkmn.atharva@gmail.com>
2024-12-10 12:40:12 -0500

    organized player objects into updatable and drawable container groups

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def main():
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	black = (0,0,0)
-	player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
+
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")		
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	
+	player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
 	
 	while True:
 		
@@ -20,10 +25,15 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		
+		for obj in updatable:
+			obj.update(dt)
+			
 		screen.fill(black)
+		
+		for obj in drawable:
+			obj.draw(screen)
+		
 		dt = clock.tick(60)/1000
-		player.update(dt)
-		player.draw(screen)
 		pygame.display.flip()
 
 if __name__ == "__main__":
