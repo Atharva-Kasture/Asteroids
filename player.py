74d52e733dc58d272ce06e3b927cc0104d2b2774
Atharva_Kasture <pkmn.atharva@gmail.com>
2024-12-11 09:28:00 -0500

    added a timer to rate of shots fired

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x,y, PLAYER_RADIUS)
         self.rotation = 0
         self.angle = 0
+        self.timer = 0
     
     # in the player class
     def triangle(self):
@@ -26,6 +27,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt, -1)
@@ -49,7 +51,10 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.timer > 0:
+            return
         new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         velocity = pygame.Vector2(0, 1).rotate(self.angle) 
         velocity *= PLAYER_SHOT_SPEED
         new_shot.velocity = velocity
+        self.timer = PLAYER_SHOOT_COOLDOWN
